ec4c3134c39f5bc0b0eaf11dfa07461f56a0f32f
yakub ahmed <39024688+yakubinfo@users.noreply.github.com>
2020-02-05 11:53:32 +0530

    Add files via upload

---
+++ fibrec1.sh
@@ -0,0 +1,19 @@
+fib()
+{
+if [[ $1 -le 1 ]]
+then
+     echo $1
+else
+     #let a=$1-1
+     #let b=$1-2
+     #a1=$(fib $a)
+     #b1=$(fib $b)
+     echo "$(($(fibo $(($1-1 | bc)))+$(fibo $(($1-2 | bc))) | bc))"
+fi
+}
+
+read -p "enter the number" n
+for i in $(seq 0 (($n-1 | bc)))
+do
+echo -n "$(fib $i)"
+done
